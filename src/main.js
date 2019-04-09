4dc5113538500657c17d66531fe4bb02f10a889e
João G. C. Krüger <jgckruger@uol.com.br>
2019-04-09 13:44:16 -0300

    Primeiro commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
-Vue.config.productionTip = false
+import 'vue-material/dist/theme/default-dark.css' // This line here
+Vue.use(VueMaterial)
+
 
 new Vue({
   router,
